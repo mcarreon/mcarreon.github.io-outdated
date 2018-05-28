f706ba4cc850f1633d7caa2b3a6dbdfc446dce11
mcarreon <carryon2095@gmail.com>
2018-05-27 19:40:36 -0700

    added mockup and began construction, add functionality

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,48 @@
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
 }
+
+$(function() {
+    $('.navBar-1').hover(function() {
+      $('#navBtn').css('color', '#B43CCB');
+    }, function() {
+      // on mouseout, reset the background colour
+      $('#navBtn').css('color', '');
+    });
+  });
+
+  $(function() {
+    $('.navBar-2').hover(function() {
+      $('#about').css('color', '#B67AFF');
+    }, function() {
+      // on mouseout, reset the background colour
+      $('#about').css('color', '');
+    });
+  });
+
+  $(function() {
+    $('.navBar-3').hover(function() {
+      $('#portfolio').css('color', '#00BDFF');
+    }, function() {
+      // on mouseout, reset the background colour
+      $('#portfolio').css('color', '');
+    });
+  });
+
+  $(function() {
+    $('.navBar-4').hover(function() {
+      $('#library').css('color', '#00FDFF');
+    }, function() {
+      // on mouseout, reset the background colour
+      $('#library').css('color', '');
+    });
+  });
+
+  $(function() {
+    $('.navBar-5').hover(function() {
+      $('#contact').css('color', '#00FFBD');
+    }, function() {
+      // on mouseout, reset the background colour
+      $('#contact').css('color', '');
+    });
+  });
